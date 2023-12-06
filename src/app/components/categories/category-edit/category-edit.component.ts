33e7840c554b813307633fe7f6c7ca8e41e77417
kriskrystev <krystev.kristiyan@gmail.com>
2023-12-07 00:00:30 +0200

    Add validation to category edit as well

--- src/app/components/categories/category-edit/category-edit.component.ts
+++ src/app/components/categories/category-edit/category-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, EnvironmentInjector, inject, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
@@ -18,6 +18,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { FormFieldErrorMessageComponent } from "../../ui/form-field-error-message/form-field-error-message.component";
 import { ERROR_STATE_MATCHER_TOKEN } from "../../../core/utils/error-state-matcher";
+import { CategoryNameAsyncValidator } from "../../../core/validation/async-validator";
 
 @Component({
   selector: 'app-category-edit',
@@ -47,13 +48,25 @@ export class CategoryEditComponent {
     private dialogRef: MatDialogRef<CategoryEditComponent>,
     private formBuilder: FormBuilder,
     private categoryService: CategoryService,
+    private categoryNameAsyncValidator: CategoryNameAsyncValidator,
     @Inject(MAT_DIALOG_DATA) public data: Category
   ) {
   }
 
   ngOnInit(): void {
+    const nameFormControl = new FormControl(this.data.name || "", {
+      validators: [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(255)
+      ],
+      asyncValidators: [
+        this.categoryNameAsyncValidator.validate.bind(this)
+      ]
+    });
+
     this.form = this.formBuilder.group({
-      name: [this.data.name || "", [Validators.required]],
+      name: nameFormControl,
       description: [this.data.description || "", Validators.required],
     })
   }
