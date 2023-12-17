c9fc9de210b79c3686e40fc9a9c4087bfd15fc5e
kriskrystev <krystev.kristiyan@gmail.com>
2023-12-17 02:14:27 +0200

    Fix edit bug where we can't save the category with the same name; introduced new method for the async validator

--- src/app/components/categories/category-edit/category-edit.component.ts
+++ src/app/components/categories/category-edit/category-edit.component.ts
@@ -1,24 +1,31 @@
 import { Component, EnvironmentInjector, inject, Inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import {
   MAT_DIALOG_DATA,
   MatDialogActions,
   MatDialogClose,
   MatDialogContent,
   MatDialogRef,
-  MatDialogTitle
-} from "@angular/material/dialog";
-import { CategoryService } from "../../../services/category.service";
-import { take } from "rxjs";
-import { ValidateForm } from "../../../core/decorators/validate-form.decorator";
-import { Category } from "../../../models/categories/response/read-category.dto";
-import { NgIf } from "@angular/common";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import { FormFieldErrorMessageComponent } from "../../ui/form-field-error-message/form-field-error-message.component";
-import { ERROR_STATE_MATCHER_TOKEN } from "../../../core/utils/error-state-matcher";
-import { CategoryNameAsyncValidator } from "../../../core/validation/async-validator";
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { CategoryService } from '../../../services/category.service';
+import { take } from 'rxjs';
+import { ValidateForm } from '../../../core/decorators/validate-form.decorator';
+import { Category } from '../../../models/categories/response/read-category.dto';
+import { NgIf } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormFieldErrorMessageComponent } from '../../ui/form-field-error-message/form-field-error-message.component';
+import { ERROR_STATE_MATCHER_TOKEN } from '../../../core/utils/error-state-matcher';
+import { CategoryNameAsyncValidator } from '../../../core/validation/async-validator';
 
 @Component({
   selector: 'app-category-edit',
@@ -38,11 +45,12 @@ import { CategoryNameAsyncValidator } from "../../../core/validation/async-valid
     FormFieldErrorMessageComponent,
   ],
   templateUrl: './category-edit.component.html',
-  styleUrl: './category-edit.component.scss'
+  styleUrl: './category-edit.component.scss',
 })
 export class CategoryEditComponent {
   form!: FormGroup;
   matcher = inject(ERROR_STATE_MATCHER_TOKEN);
+  initialCategoryName = '';
 
   constructor(
     private dialogRef: MatDialogRef<CategoryEditComponent>,
@@ -50,33 +58,37 @@ export class CategoryEditComponent {
     private categoryService: CategoryService,
     private categoryNameAsyncValidator: CategoryNameAsyncValidator,
     @Inject(MAT_DIALOG_DATA) public data: Category
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    const nameFormControl = new FormControl(this.data.name || "", {
+    this.initialCategoryName = this.data.name;
+    const asyncValidator = this.categoryNameAsyncValidator
+      .validateWithName(this.initialCategoryName)
+      .bind(this);
+
+    const nameFormControl = new FormControl(this.data.name || '', {
       validators: [
         Validators.required,
         Validators.minLength(1),
-        Validators.maxLength(255)
+        Validators.maxLength(255),
       ],
-      asyncValidators: [
-        this.categoryNameAsyncValidator.validate.bind(this)
-      ]
+      asyncValidators: [asyncValidator],
+      // asyncValidators: [asyncValidator()],
     });
 
     this.form = this.formBuilder.group({
       name: nameFormControl,
-      description: [this.data.description || "", Validators.required],
-    })
+      description: [this.data.description || '', Validators.required],
+    });
   }
 
   @ValidateForm()
   confirm(): void {
-    this.categoryService.updateCategory(this.data.id, this.form.value).pipe(
-      take(1)
-    ).subscribe({
-      next: () => this.dialogRef.close(true)
-    });
+    this.categoryService
+      .updateCategory(this.data.id, this.form.value)
+      .pipe(take(1))
+      .subscribe({
+        next: () => this.dialogRef.close(true),
+      });
   }
 }
