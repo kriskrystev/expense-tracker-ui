d3213dea8cc03e2ab6b6e118e609189ccb67a1cb
kriskrystev <krystev.kristiyan@gmail.com>
2024-02-22 17:27:25 +0200

    Consume the login endpoint and redirect after login

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,14 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   loggedIn$ = new BehaviorSubject(false);
+  url = 'http://localhost/api';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private http: HttpClient) {
     if (this.hasAccessToken()) {
       this.loggedIn$.next(true);
     }
@@ -25,5 +27,19 @@ export class AuthService {
 
   initLogin() {
     this.router.navigate(['/login']);
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post<{ access_token: string }>('http://localhost/api/auth/login', {
+        username,
+        password,
+      })
+      .pipe(
+        tap((payload: { access_token: string }) => {
+          this.setAccessToken(payload.access_token);
+          this.router.navigate(['/dashboard']);
+        })
+      );
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,15 +11,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ERROR_STATE_MATCHER_TOKEN } from '../core/utils/error-state-matcher';
+import { FormFieldErrorMessageComponent } from '../components/ui/form-field-error-message/form-field-error-message.component';
+import { ValidateForm } from '../core/decorators/validate-form.decorator';
+import { AuthService } from '../core/auth/auth.service';
 
 const core = [NgIf, FormsModule, ReactiveFormsModule];
 
 const material = [MatFormFieldModule, MatInputModule, MatButtonModule];
+const internal = [FormFieldErrorMessageComponent];
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [...core, ...material],
+  imports: [...core, ...material, ...internal],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -27,12 +31,19 @@ export class LoginComponent implements OnInit {
   form!: FormGroup;
   matcher = inject(ERROR_STATE_MATCHER_TOKEN);
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(2)]],
       password: ['', [Validators.required, Validators.minLength(8)]],
     });
+  }
+
+  @ValidateForm()
+  login(): void {
+    this.authService
+      .login(this.form.value.username, this.form.value.password)
+      .subscribe();
   }
 }
