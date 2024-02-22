e6c3a9c59b230e6a51ed99a29abceaef6b2aaf89
kriskrystev <krystev.kristiyan@gmail.com>
2024-02-22 17:14:52 +0200

    Layout and initialize login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ERROR_STATE_MATCHER_TOKEN } from '../core/utils/error-state-matcher';
+
+const core = [NgIf, FormsModule, ReactiveFormsModule];
+
+const material = [MatFormFieldModule, MatInputModule, MatButtonModule];
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [...core, ...material],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  matcher = inject(ERROR_STATE_MATCHER_TOKEN);
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(2)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
 }
